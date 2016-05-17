0f2def098992d0d8ff6c95e876560400281db44e
Драганчук Алена Сергеевна <ri57s05@awp09.csc.local>
2016-05-17 10:32:45 +0600

    Change file's name

---
+++ src/mode.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include "mode.h"
+
+int slen(char str[]){
+	int i;
+	int len;
+	for (i=0;str[i]!='\0';i++);
+		len=i;
+	return len;
+}
+
+int mode1(int lenght, char password[]) {
+	char AZ[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	char az[] = "qwertyuiopasdfghjklzxcvbnm";	
+	srand(time(0));
+	for (int i = 0; i < lenght - 2; i + 2){
+		password[i] = AZ[rand()%slen(AZ)+1];
+		password[i+1] = az[rand()%slen(az)+1];
+	}
+	return 0;	
+}
+
+int mode2(int lenght, char password[]) {
+	char num[] = "0123456789";	
+	srand(time(0));
+	for (int i = 0; i < lenght; i++){
+		password[i] = num[rand() % slen(num) + 1];
+	}
+	return 0;
+}
+
+int mode4(int lenght, char password[]) {
+	char ch[] = "%*?!@#";
+	char AZ[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	char az[] = "qwertyuiopasdfghjklzxcvbnm";
+	char num[] = "0123456789";	
+	srand(time(0));
+	for (int i = 0; i < lenght-4; i+2){
+		password[i] = AZ[rand()%slen(AZ)+1];
+		password[i+2] = az[rand()%slen(az)+1];
+		password[i+1] = ch[rand()%slen()+1];
+		password[i+4] = num[rand()%slen()+1];
+	}
+	return 0;	
+}
